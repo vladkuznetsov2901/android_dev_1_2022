72ef526b60114847ef76567cc8e870fd95b5db14
Vlad Kuznetsov <vladkuznetsov2901@gmail.com>
2023-06-23 16:00:41 +0300

    Dz m5

--- m5_quiz_resources/app/src/main/java/com/example/skillbox_hw_quiz/MainActivity.kt
+++ m5_quiz_resources/app/src/main/java/com/example/skillbox_hw_quiz/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.example.skillbox_hw_quiz
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Picture
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.PictureDrawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.skillbox_hw_quiz.databinding.MainActivityBinding
 import com.example.skillbox_hw_quiz.ui.main.MainFragment
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val binding = MainActivityBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(binding.root)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+
+        binding.welcomeImage.setImageResource(R.drawable.welcome_picture)
+
     }
+
+
 }
